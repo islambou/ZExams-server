8d3d0c9a28be96b93736d563b62f450da110b7a8
Islam <islambou@gmail.com>
2018-11-12 14:26:40 +0100

    duplacte the api call

--- routes/question.js
+++ routes/question.js
@@ -24,6 +24,10 @@ router.post("/get", function(req, res) {
     console.log(e);
     res.sendStatus(500);
   });
+  result.then(v => res.send(v)).catch(e => {
+    console.log(e);
+    res.sendStatus(500);
+  });
 });
 
 /*delete questions */
